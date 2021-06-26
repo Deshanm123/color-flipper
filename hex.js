fdfa61c2f1ad73ff01b67d9937ae5f71f46f99ed
Deshan <deshanm123@gmail.com>
2021-06-26 22:37:08 +0530

    implemented hex color generation on the fly

--- hex.js
+++ hex.js
@@ -8,8 +8,12 @@ btn.addEventListener('click',()=>{
     // console.log(document.body);
     let hexColor = '#';
     for(let i=0;i<6;i++){
-        hexColor += hex[0];
+        hexColor += hex[getRandomNumber()];
     }
     color.textContent = hexColor;
     document.body.style.backgroundColor = hexColor;
 });
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * hex.length);
+}
